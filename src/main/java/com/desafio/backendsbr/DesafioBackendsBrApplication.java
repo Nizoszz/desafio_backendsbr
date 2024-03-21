827839acca9106ba40886b104b643da45b5f840d
Andrew <andrewairamdasilva@gmail.com>
2024-03-21 16:09:49 -0300

    feat: initialize project class

---
+++ src/main/java/com/desafio/backendsbr/DesafioBackendsBrApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.backendsbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendsBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendsBrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/backendsbr/DesafioBackendsBrApplicationTest.java
@@ -0,0 +1,13 @@
+package com.desafio.backendsbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendsBrApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
