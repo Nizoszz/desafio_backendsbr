f4f510bbc09289a480f9ff1776fa76c49f59ce47
Andrew <andrewairamdasilva@gmail.com>
2024-03-21 16:09:05 -0300

    feat: request and response DTOS

---
+++ src/main/java/com/desafio/backendsbr/dtos/OperationRequestDTO.java
@@ -0,0 +1,5 @@
+package com.desafio.backendsbr.dtos;
+
+public record OperationRequestDTO(String userDocument, String creditCardToken, Long value) {
+
+}
---
+++ src/main/java/com/desafio/backendsbr/dtos/OperationResponseDTO.java
@@ -0,0 +1,4 @@
+package com.desafio.backendsbr.dtos;
+
+public record OperationResponseDTO(Long id, String userDocument, String creditCardToken, Long value) {
+}
